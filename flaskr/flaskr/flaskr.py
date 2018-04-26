6b2fce07dfe2f7033ab64c4be25c49181903d4ba
Abdulrahman94 <bousbait1994@gmail.com>
2018-04-27 01:03:31 +0300

    update flaskr file and add some comments

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -1,10 +1,17 @@
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
-from .text_generator import Writer
+from .text_generator import Writer, get_prediction
 
 
-writer = Writer()
-writer.load("model_info/")
+forward_writer= Writer()
+forward_writer.load(path='model_info/', name='forward')
+#*************note begin*************
+#here sould be the initialization of backward_writer
+# the code is:
+#backward_writer= Writer()
+#backward_writer.load(path='model_info/', name='backward')
+#*************note end*************
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.update(dict(
@@ -33,10 +40,16 @@ def oktop():
         init = request.form['words']
         if int(request.form['num']) > 0 :
             num = int(request.form['num'])
-            result = writer.write(1, words=init.split(), number_of_word=num)
+            result = forward_writer.write(1, words=init.split(), number_of_word=num)
     else:
-        result = writer.write(0, number_of_word=400)
+        result = forward_writer.write(0, number_of_word=400)
     return render_template('result.html', result=result)
-
+#*************note begin*************
+#here should be a method called (predict) and work like this:
+#First the data should be taken from the user as list of string as in (oktop) method
+#Second applaying this code:
+#result = get_prediction(forward_writer, backward_writer, data)
+#---> where (data) is user input and (result) is output
+#*************note end*************
 if __name__ == '__main__':
     app.run()
